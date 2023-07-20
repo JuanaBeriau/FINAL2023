5f9e92c8115f407ddb678c49ab805a65bdf807d8
JuanaBeriau <138032287+JuanaBeriau@users.noreply.github.com>
2023-07-20 11:41:15 -0300

    Create class_entrada.py

---
+++ Carpeta 1/class_entrada.py
@@ -0,0 +1,17 @@
+class Entrada:
+
+    def __init__(self, id, estadio, partido, precio, sector) -> None:
+        self.id = id
+        self.estadio = estadio
+        self.partido = partido
+        self.precio = precio
+        self.sector = sector
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'estadio': self.estadio,
+            'partido': self.partido,
+            'precio': self.precio,
+            'sector': self.sector
+        }
